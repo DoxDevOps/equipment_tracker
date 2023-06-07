771da6c8378057c168cfd2a2b964841ddba983e4
mnaboti <mnaboti@pedaids.org>
2023-06-07 11:45:27 +0200

    modified equipment model

---
+++ equipment_tracker/equipments/__init__.py
@@ -0,0 +1,8 @@
+# from .models import Equipment
+
+
+class Itemlocation:
+    LOCATION_CHOICES = (
+        ("warehouse", "warehouse"),
+        ("facility", "facility"),
+    )
--- equipment_tracker/equipments/models.py
+++ equipment_tracker/equipments/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 from equipment_tracker.common.base import BaseModel
 
+from . import Itemlocation
+
 # Create your models here.
 
 
 class Equipment(BaseModel):
     name = models.CharField(max_length=100, null=True)
-    description = models.TextField(max_length=100, null=True)
+    description = models.TextField(null=True)
     serial_number = models.CharField(max_length=50, null=True)
-    location = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=100, null=True, choices=Itemlocation.LOCATION_CHOICES)
     status = models.CharField(max_length=50, null=True)
 
     def __str__(self):

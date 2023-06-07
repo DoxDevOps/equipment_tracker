d087742d31f6c448c8cb5e7ea27269db2687ea5c
Owen Onions <owenonions@gmail.com>
2023-06-07 13:49:38 +0200

    Add Stock, Warehouse, WareHouseStock, Facility and FacilityStock

--- equipment_tracker/equipments/models.py
+++ equipment_tracker/equipments/models.py
@@ -1,21 +1,51 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from equipment_tracker.common.base import BaseModel
-
-from . import ItemLocation
-
-# Create your models here.
+from equipment_tracker.equipments import ItemLocation
 
 
-class Equipment(BaseModel):
-    name = models.CharField(max_length=100, null=True)
+class Stock(BaseModel):
+    """Item or material"""
+
+    name = models.CharField(max_length=100, blank=False, help_text="The name of the equipment", null=True)
     description = models.TextField(null=True)
-    serial_number = models.CharField(max_length=50, null=True)
-
-
-class Location(models.CharField):
-    name = models.CharField(
-        null=False,
+    location = models.CharField(
+        _("Current location"),
         choices=ItemLocation.CHOICES,
+        max_length=50,
+        null=False,
         default=ItemLocation.WAREHOUSE,
     )
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Warehouse(BaseModel):
+    """Stores equipment"""
+
+    name = models.CharField(max_length=100, blank=False, help_text="The name of the warehouse")
+    description = description = models.TextField()
+
+
+class WarehouseStock(BaseModel):
+    """Stock at a warehouse"""
+
+    stock = models.ForeignKey("Stock", on_delete=models.CASCADE)
+    warehouse = models.ForeignKey("Warehouse", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(_("Quantity"), blank=False)
+
+
+class Facility(BaseModel):
+    """Health facility"""
+
+    name = models.CharField(max_length=100, blank=False, help_text="The name of the facility")
+
+
+class FacilityStock(BaseModel):
+    """Stock at a facility"""
+
+    stock = models.ForeignKey("Stock", on_delete=models.CASCADE)
+    facility = models.ForeignKey("Facility", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(_("Quantity"), blank=False)
